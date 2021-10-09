19fa3905ae96486582af7f38be703be44df1bb30
vxfury <vxfury.com@gmail.com>
2021-10-10 01:36:22 +0800

    eliminate warnings

--- tests/benchmark.h
+++ tests/benchmark.h
@@ -3,28 +3,28 @@
 #include <time.h>
 #include <math.h>
 
-#define BENCHER(prefix, todo, N, REPEAT)                                                                   \
-    double prefix##avg, prefix##stddev;                                                                    \
-    do {                                                                                                   \
-        double durations[N];                                                                               \
-        for (unsigned __i = 0; __i < N; __i++) {                                                           \
-            struct timespec ts, te;                                                                        \
-            clock_gettime(CLOCK_MONOTONIC, &ts);                                                           \
-            for (unsigned int __j = 0; __j < REPEAT; __j++) { todo; }                                      \
-            clock_gettime(CLOCK_MONOTONIC, &te);                                                           \
-            durations[__i] =                                                                               \
-                ((te.tv_sec - ts.tv_sec) * 1e9 + (te.tv_nsec - ts.tv_nsec)) / static_cast<double>(REPEAT); \
-        }                                                                                                  \
-        long double __t = 0;                                                                               \
-        for (unsigned __i = 0; __i < N; __i++) { __t += static_cast<long double>(durations[__i]); }        \
-        prefix##avg = __t / N;                                                                             \
-                                                                                                           \
-        for (unsigned __i = 0; __i < N; __i++) {                                                           \
-            double diff = durations[__i] - prefix##avg;                                                    \
-            __t += static_cast<long double>(diff * diff);                                                  \
-        }                                                                                                  \
-        __t /= N;                                                                                          \
-        prefix##stddev = sqrt(__t);                                                                        \
+#define BENCHER(prefix, todo, N, REPEAT)                                                            \
+    double prefix##avg, prefix##stddev;                                                             \
+    do {                                                                                            \
+        double durations[N];                                                                        \
+        for (unsigned __i = 0; __i < N; __i++) {                                                    \
+            struct timespec ts, te;                                                                 \
+            clock_gettime(CLOCK_MONOTONIC, &ts);                                                    \
+            for (unsigned int __j = 0; __j < REPEAT; __j++) { todo; }                               \
+            clock_gettime(CLOCK_MONOTONIC, &te);                                                    \
+            durations[__i] = (te.tv_sec - ts.tv_sec) * (1e9 / static_cast<double>(REPEAT))          \
+                             + (te.tv_nsec - ts.tv_nsec) / static_cast<double>(REPEAT);             \
+        }                                                                                           \
+        long double __t = 0;                                                                        \
+        for (unsigned __i = 0; __i < N; __i++) { __t += static_cast<long double>(durations[__i]); } \
+        prefix##avg = static_cast<double>(__t / N);                                                 \
+                                                                                                    \
+        for (unsigned __i = 0; __i < N; __i++) {                                                    \
+            double diff = durations[__i] - prefix##avg;                                             \
+            __t += static_cast<long double>(diff * diff);                                           \
+        }                                                                                           \
+        __t /= N;                                                                                   \
+        prefix##stddev = static_cast<double>(sqrt(__t));                                            \
     } while (0)
 
 template <class Tp>
@@ -49,7 +49,8 @@ inline void DoNotOptimize(Tp &value)
 
 class Bencher {
   public:
-    Bencher(const std::string &name, std::function<bool(void)> task, size_t repeat = 0, const size_t N = 5) : name(name)
+    Bencher(const std::string &name, std::function<bool(void)> task, size_t repeat = 0, const size_t N = 5)
+        : title(name)
     {
         if (repeat == 0) { // auto checking
             struct timespec ts, te;
@@ -57,12 +58,12 @@ class Bencher {
             if (!task()) { return; }
             clock_gettime(CLOCK_MONOTONIC, &te);
 
-            uint64_t delta = (te.tv_sec - ts.tv_sec) * 1e9 + (te.tv_nsec - ts.tv_nsec);
+            time_t delta = (te.tv_sec - ts.tv_sec) * 1'000'000'000 + (te.tv_nsec - ts.tv_nsec);
             if (delta <= 1000) {
                 repeat = 1000;
             } else {
                 // Aim for about 100ms between time checks.
-                repeat = static_cast<unsigned>(1e8 / delta);
+                repeat = 100'000'000 / delta;
                 if (repeat > 1000) {
                     repeat = 1000;
                 } else if (repeat < 1) {
@@ -90,11 +91,11 @@ class Bencher {
 
     ~Bencher()
     {
-        std::cout << name << ": avg = " << avg << ", stddev = " << stddev << std::endl;
+        std::cout << title << ": avg = " << avg << ", stddev = " << stddev << std::endl;
     }
 
   private:
     double avg;
     double stddev;
-    std::string name;
+    std::string title;
 };
